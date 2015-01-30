f37ce26549cac9b849c20a5bc344f238f7a9b262
Ralph Winzinger <ralph.winzinger@senacor.com>
2015-01-30 18:31:17 +0100

    Dateien 2014 / 2015 entfernt

--- 2014/workspace/com.senacor.mdsd.hh.xtendexample/src/foo/bar/NameProvider.java
+++
@@ -1,7 +0,0 @@
-package foo.bar;
-
-public class NameProvider {
-	public static String getName() {
-		return "Ralph";
-	}
-}
--- 2015/servicesproject/base/src/main/java/com/senacor/services/base/DAO.java
+++
@@ -1,7 +0,0 @@
-package com.senacor.services.base;
-
-/**
- * Created by WinzingeR on 20.01.2015.
- */
-public interface DAO {
-}
