77a89fbd3b7144941090ac00a2ef3fb1cb2453c2
Winzinger, Ralph <Ralph.Winzinger@senacor.com>
2016-01-06 07:19:53 +0100

    sample code snippets

---
+++ 2016/snippets/App.java
@@ -0,0 +1,13 @@
+package unihh.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
